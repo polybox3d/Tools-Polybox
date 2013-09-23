1b1afcf425e566ed92fe2fe8417dff05ac146439
poly <poly@polybox.(none)>
2013-09-23 15:48:22 +0200

    Complete linuxcnc script install

--- install-cnc-2.sh
+++ install-cnc-2.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#Is your kernel patched ? (real time with Xenomai)
+if [ `uname -r |grep xenomai | wc -l` -gt 0  ];
+then
+    echo "======> Your kernel is patched for RealTime. I'm goint to process LinuxCNC installation and configuration now."
+else
+    echo "======> I can't execute this script. Your kernel need to be patched fro realTime by Xenomai."
+    echo "======> Try to execute ./install-cnc.sh 1st."
+    exit 0
+fi
+
+
 #===Fetch===
 wget http://static.mah.priv.at/public/xenomai-debs/libxenomai1_2.6.1_i386.deb
 wget http://static.mah.priv.at/public/xenomai-debs/libxenomai-dev_2.6.1_i386.deb
@@ -25,6 +36,12 @@ cd ..
 #This will show a list of packages missing (if any)
 dpkg-checkbuilddeps
 
+#===Compile and config LinuxCNC===
+cd src
+sh autogen.sh
+./configure 
+make
+sudo make setuid
 #===Add right===
 sudo adduser "$USER" xenomai
 sudo adduser "$USER" kmem
