2ee6cda0c37c2df813f8cd2459ff51536ee4cc0f
poly <poly@polybox.(none)>
2013-09-23 20:15:08 +0200

    add some modification about packages and apt-get askin

--- install-cnc-2.sh
+++ install-cnc-2.sh
@@ -11,10 +11,11 @@ else
 fi
 
 
+cd /tmp/
 #===Fetch===
 wget http://static.mah.priv.at/public/xenomai-debs/libxenomai1_2.6.1_i386.deb
 wget http://static.mah.priv.at/public/xenomai-debs/libxenomai-dev_2.6.1_i386.deb
-
+wget http://static.mah.priv.at/public/xenomai-debs/xenomai-runtime_2.6.1_i386.deb
 #===Install After rebbot and Xenomai up===
 
 sudo dpkg -i libxenomai1_2.6.1_i386.deb
@@ -22,9 +23,9 @@ sudo dpkg -i libxenomai-dev_2.6.1_i386.deb
 sudo dpkg -i xenomai-runtime_2.6.1_i386.deb
 
 #===Get and install LinuxCNC===
-
-git clone git://git.mah.priv.at/emc2-dev.git linuxcnc-rtos
-cd linuxcnc-rtos
+cd ~/
+git clone git://git.mah.priv.at/emc2-dev.git linuxcnc
+cd linuxcnc
 #By default, you will get files from "master", which is not what we want - there is a good branch: 
 git branch --track rtos-integration-preview3 origin/rtos-integration-preview3
 git checkout rtos-integration-preview3
@@ -40,7 +41,17 @@ dpkg-checkbuilddeps
 cd src
 sh autogen.sh
 ./configure 
+if [ $? -ne 0 ]; then
+    echo "=========>./configure command ERROR : $retval"
+    echo "=========> Stop.]"
+    exit
+fi
 make
+if [ $? -ne 0 ]; then
+    echo "=========>make command ERROR : $retval"
+    echo "=========> Stop.]"
+    exit
+fi
 sudo make setuid
 #===Add right===
 sudo adduser "$USER" xenomai
--- install-cnc.sh
+++ install-cnc.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 
-sudo apt-get install python-gtk2 libglade2-dev python-glade2 python-gnome2
+sudo apt-get -y install python-gtk2 libglade2-dev python-glade2 python-gnome2
+sudo apt-get -y install libpth-dev tcl8.5-dev tk8.5-dev bwidget libxaw7-dev libncurses-dev python-support libgnomeprintui2.2-dev texlive-lang-cyrillic libudev-dev
 
+cd /tmp/
 #===Fetch===
 wget http://static.mah.priv.at/public/xenomai-debs/linux-headers-3.2.21-xenomai+_0.4_i386.deb
 wget http://static.mah.priv.at/public/xenomai-debs/linux-image-3.2.21-xenomai+_0.4_i386.deb
