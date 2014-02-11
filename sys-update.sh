2be09499f481e507817e624632ba21d3322a5ed7
poly <poly@polybox.(none)>
2014-02-11 11:33:29 +0100

    update sys-update job

--- sys-update.sh
+++ sys-update.sh
@@ -1,3 +1,33 @@
-#!/bin/bash
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          system update&upgrade
+# Required-Start:
+# Required-Stop:
+# Default-Start:     always execute
+# Default-Stop:      
+# Short-Description: Execute a full systeme update and upgrade without user intervention.
+# Description:
+### END INIT INFO
 
-(apt-get update && apt-get -y upgrade) > /dev/null
+PATH=/sbin:/usr/sbin:/bin:/usr/bin
+
+do_start () {
+    apt-get update && apt-get -y upgrade
+    date +"%d-%m-%y" > /home/poly/lastSystUpgrade
+}
+
+case "$1" in
+  start)
+	do_start
+	;;
+  restart|reload|force-reload)
+	echo "Error: argument '$1' not supported" >&2
+	exit 3
+	;;
+  stop)
+	;;
+  *)
+	echo "Usage: $0 start|stop" >&2
+	exit 3
+	;;
+esac
