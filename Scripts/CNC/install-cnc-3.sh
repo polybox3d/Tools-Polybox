126910dc808758fbe4e5d69bc8dd3d94399732ed
SkapiN <skapinthefourb@gmail.com>
2015-03-06 22:57:05 +0100

    Update install-cnc-3.sh
    update for deainlinuxcnc

--- Scripts/CNC/install-cnc-3.sh
+++ Scripts/CNC/install-cnc-3.sh
@@ -6,7 +6,7 @@ source $MY_DIR/../bin/utils.sh
 
 LINUX_CNC_FOLDER=~/linuxcnc
 
-echoC "$GREEN" "Okay, We gonna check your kernel and install LinuxCNC !.\n"
+echoC "$GREEN" "Okay, We gonna check your kernel and LinuxCNC !.\n"
 resetC
 
 #Is your kernel patched ? (real time with RTAI)
@@ -15,27 +15,9 @@ then
     echo "======> Your kernel is patched for RealTime with RTAI. I'm goint to process LinuxCNC installation and configuration now."
 else
     echo "======> I can't execute this script. Your kernel need to be patched for realTime by RTAI."
-    echo "======> Try to execute ./install-cnc.sh 1st."
+    echo "======> install another kernel, or switch branch from Git repo"
     exit 0
 fi
-
-
-
-echoC "$BLU" "##############################################################\n
-We need to create a file called /etc/apt/sources.list.d/linuxcnc-buildbot.list ,\n
-containing one of these lines (depending on which version of linuxcnc you want): \n\n
-    deb http://buildbot.linuxcnc.org precise 2.6-rt \n
-    deb http://buildbot.linuxcnc.org precise master-rt \n
-    deb http://buildbot.linuxcnc.org precise v2.5_branch-rt \n\n
-The current lastest stable version is 2.5. (Jul 2014)\n
-##########################################\n"
-resetC
-
-sudo sh -c 'echo "deb http://buildbot.linuxcnc.org precise v2.5_branch-rt" >> /etc/apt/sources.list.d/linuxcnc-buildbot.list'
-
-sudo apt-get -y update
-sudo apt-get -y --force-yes install linuxcnc
-print_check_error
 
 #===Copy miling configuration file===
 mkdir $LINUX_CNC_FOLDER
