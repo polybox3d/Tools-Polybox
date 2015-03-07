99330f258e8d994363ecb896346a1484f8fe6f2d
poly <poly@localhost.localdomain>
2015-03-07 14:50:36 +0100

    out of date

--- Scripts/install-scan.sh
+++ Scripts/install-scan.sh
@@ -12,12 +12,41 @@ source $MY_DIR/bin/utils.sh
 
 echoC "$BLU" "=====Scanner 3D"
 resetC
-
-sudo add-apt-repository -y ppa:v-launchpad-jochen-sprickerhof-de/pcl
-sudo apt-get -y update
-print_check_error
-sudo apt-get -y install libpcl-all uvccapture
-print_check_error
 sudo adduser "$USER" dialout
+
+
+echoC "$GREEN" "*******************"
+echo  " Out of date script. Nothing to do. "
+echo "Continue."
+echo "*******************"
+resetC
+
+print_bye
+
+exit 0
+## for Debian
+if [ `lsb_release -d | grep Debian | wc -l` -gt 0  ];
+then
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 19274DEF
+	sudo sh -c 'echo "deb http://ppa.launchpad.net/v-launchpad-jochen-sprickerhof-de/pcl/ubuntu maverick main" > /etc/apt/sources.list.d/pcl.list'
+	sudo apt-get update
+	sudo apt-get  -t wheezy-backports -y install libpcl-all	uvccapture
+	print_check_error
+
+## for Ubuntu
+elif [ `lsb_release -d | grep Ubuntu | wc -l` -gt 0  ];	
+then
+	sudo add-apt-repository -y ppa:v-launchpad-jochen-sprickerhof-de/pcl
+	sudo apt-get -y update
+	print_check_error
+	sudo apt-get -y install libpcl-all uvccapture
+	print_check_error
+else
+	echoC "$RED" "=====>ERROR. You are not using Debian or Ubuntu... We don't know how ot install PCL package on your OS"
+	echo "Script aborted."
+	resetC
+	exit 1
+fi
+
 
 exit 0
